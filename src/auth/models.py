f504623415d5e8e69a31d00544906582096f00c9
mikhail <mishaprizby@gmail.com>
2023-12-27 12:55:12 +0300

    Added login

--- src/auth/models.py
+++ src/auth/models.py
@@ -16,6 +16,6 @@ class TokenTable(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id : Mapped[int]
     access_toke : Mapped[str] =mapped_column(String(450), unique=True, nullable=False)
-    access_toke : Mapped[str] =mapped_column(String(450), unique=True, nullable=False)
+    refresh_toke : Mapped[str] =mapped_column(String(450), unique=True, nullable=False)
     status : Mapped[bool]
-    created_date : Mapped[datetime.datetime]
+    created_date : Mapped[datetime.datetime] = mapped_column(default= datetime.datetime.now())
--- src/auth/router.py
+++ src/auth/router.py
@@ -1,12 +1,12 @@
 import json
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 
-
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from base import get_async_session
-from .utils import get_hashed_password
-from .models import User
-from .schemas import UserCreate
+from .utils import get_hashed_password, verify_password, create_access_token, create_refresh_token
+from .models import User, TokenTable
+from .schemas import UserCreate, TokenCreate, TokenSchema, requestdetails
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -32,6 +32,31 @@ async def register_users(
             status_code=500, detail={"status": "error", "data": None, "details": None}
         )
 
+@router.post('/login' ,response_model=TokenSchema)
+async def login(request: requestdetails, session: AsyncSession = Depends(get_async_session)):
+    query = select(User).filter(User.email == request.email)
+    result = await session.execute(query)
+    user = result.scalars().first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email")
+    hashed_pass = user.password
+    if not verify_password(request.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+    
+    access=create_access_token(user.id)
+    refresh = create_refresh_token(user.id)
+
+    token_db = TokenTable(user_id=user.id,  access_toke=access,  refresh_toke=refresh, status=True)
+    session.add(token_db)
+    await session.commit()
+    await session.refresh(token_db)
+    return {
+        "access_token": access,
+        "refresh_token": refresh,
+    }
 
 
 
